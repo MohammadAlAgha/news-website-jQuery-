66f4ebd5dfd2fd8a747f22fa04180d37b7d973d9
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-13 21:08:53 +0200

    Styled the incomming data

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,16 @@
 $(document).ready(function () {});
+
+{
+  /* <div class="news">
+        <h2 class="title">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit et
+          nihil quaerat eveniet
+        </h2>
+        <div class="description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ad
+          nihil dolorum quisquam, voluptate voluptatibus laudantium optio quas
+          delectus ipsa omnis sequi aliquid, quaerat magni dolores, porro natus
+          illum id!
+        </div>
+      </div> */
+}
